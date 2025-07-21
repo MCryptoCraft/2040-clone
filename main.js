15ab0637a4049ea0350d45f087b1516ece92a4a3
Ajmal <tasc1000op@gmail.com>
2025-07-21 23:12:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-brother complete tthis
+const board = document.getElementById("game-board");
+
+function createTile(value = "") {
+  const tile = document.createElement("div");
+  tile.className = "tile";
+  tile.textContent = value;
+  return tile;
+}
+
+function initBoard() {
+  board.innerHTML = "";
+  for (let i = 0; i < 16; i++) {
+    board.appendChild(createTile());
+  }
+}
+
+window.onload = initBoard;
